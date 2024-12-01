83e79985056d3bfed1e56ed8ad7f9c129b2e1830
pgvalle <pgvalle.nogueira@gmail.com>
2024-12-01 16:43:50 -0300

    test

--- client.py
+++ client.py
@@ -2,17 +2,17 @@ import xmlrpc.client
 import sys
 
 
-if __name__ == "__main__":
-    binder = xmlrpc.client.ServerProxy(f'http://localhost:12345')
-    rpchat_port = binder.lookup_procedure('rpchat')
+if __name__ == '__main__':
+    binder = xmlrpc.client.ServerProxy(f'http://localhost:1234')
+    host, port = binder.lookup_procedure('rpchat')
 
-    if rpchat_port is None:
+    if [host, port] == [None, None]:
         print('This service was not registered')
         exit(1)
 
     # Cria um cliente que se conecta ao servidor de calculadora na porta descoberta
-    rpchat = xmlrpc.client.ServerProxy(f'http://localhost:{rpchat_port}')
+    rpchat = xmlrpc.client.ServerProxy(f'http://{host}:{port}')
     rpchat.create_room('babolei')
     result = rpchat.join_room('babolei', 'jacan')
     print(result)
-    #rpchat.leave_room('babolei', result)
+    rpchat.leave_room('babolei', result[0])
