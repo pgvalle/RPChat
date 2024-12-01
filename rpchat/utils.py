9a42d6b8f99678379948aca77bbb7155fbc1811c
pgvalle <pgvalle.nogueira@gmail.com>
2024-12-01 15:42:37 -0300

    removed % from token symbols

--- rpchat/utils.py
+++ rpchat/utils.py
@@ -1,6 +1,6 @@
 from random import choices
 
-__TOKEN_CHARS = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$%&*')
+__TOKEN_CHARS = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$&*')
 
 def create_token(size):
     token_as_list = choices(__TOKEN_CHARS, k=size)
