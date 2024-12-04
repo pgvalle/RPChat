1ab8d143341ad61459ac212703ccd7cb5a6eab67
pgvalle <pgvalle.nogueira@gmail.com>
2024-12-04 14:38:17 -0300

    idk

--- client.py
+++ client.py
@@ -1,9 +1,35 @@
 import xmlrpc.client
-import sys
+import sys, time, threading
+
+messages = []
+# urwid
+exit_evt = threading.Event()
+
+def recv_msgs():
+    while not exit_evt.is_set():
+        start = time.ctime()
+
+        
+
+        delta = time.ctime() - start
+        
+        if delta < 2:
+            time.sleep(2 - delta)
 
 
-if __name__ == '__main__':
-    binder = xmlrpc.client.ServerProxy(f'http://localhost:1234')
+def main():
+    if len(sys.argv) < 3:
+        print('Expected host and port')
+        return
+    
+    # cli arguments parsing
+    try:
+        binder_host, binder_port = sys.argv[1], int(sys.argv[2])
+    except Exception as e:
+        print(f'Error when parsing arguments: {e}')
+        return
+
+    binder = xmlrpc.client.ServerProxy(f'http://{binder_host}:{binder_port}')
     host, port = binder.lookup_procedure('rpchat')
 
     if [host, port] == [None, None]:
