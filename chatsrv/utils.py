0f81a016666db99dc69d083fa1dff651bf16af95
pgvalle <pgvalle.nogueira@gmail.com>
2024-12-04 14:20:11 -0300

    utils.py out of play

--- chatsrv/utils.py
+++
@@ -1,7 +0,0 @@
-from random import choices
-
-__TOKEN_CHARS = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?@#$&*')
-
-def create_token(size):
-    token_as_list = choices(__TOKEN_CHARS, k=size)
-    return ''.join(token_as_list)
