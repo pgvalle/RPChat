94e12c01086097db4287c1cd897440c7a2ea41e3
pgvalle <pgvalle.nogueira@gmail.com>
2024-12-18 22:25:42 -0300

    asdfasdf

--- tui.py
+++ tui.py
@@ -5,29 +5,33 @@ _is_windows = platform.system() == 'Windows'
 
 if _is_windows:
     # enable ascii sequences
-    import msvcrt, ctypes, time
+    import msvcrt, ctypes
     kernel32 = ctypes.windll.kernel32
     kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 else:
-    import select
+    import os
+    fd = stdin.fileno()
+    os.set_blocking(fd, False)
 
 def init():
     stdout.write('\x1b[?1049h\x1b[?47h')
     stdout.flush()
 
-def getkey(timeout=0.01):
-    if _is_windows:
-        time.sleep(timeout)
-        if msvcrt.kbhit():
-            ch = msvcrt.getch()
-            return ch.decode()
-    else:
-        if select.select([stdin], [], [], 0.0)[0]:
-            return stdin.read(1)
+def getkey():
+    if _is_windows and msvcrt.kbhit():
+        ch = msvcrt.getch()
+        return ch.decode()
     
+    if not _is_windows:
+        line = stdin.read().strip()
+        return line
+        
     return ''
 
 def terminate():
+    fd = stdin.fileno()
+    os.set_blocking(fd, False)
+    
     stdout.write('\x1b[?47l\x1b[?1049l')
     stdout.flush()
     exit(0)
